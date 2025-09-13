ff26cc4c9f015a7215dd6537c184466cf7d8bcaf
Backspac3_N00B <leandro.oliveiracustodio02@hotmail.com>
2025-09-13 20:38:10 -0300

    ajustando teste ex common prefix
    Co-Authored-By: TheLeleo <99210769+theleleo@users.noreply.github.com>

---
+++ 14-longest-common-prefix.cpp
@@ -0,0 +1,50 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = ""; // resultado inicial vazio
+        // ponto para você colocar breakpoints ou prints depois
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<string> test = {"pao", "padaria", "padeiro", "pedra", "pedrao"};
+
+    string res = sol.longestCommonPrefix(test);
+    cout << "Resultado: " << res << endl;
+
+    return 0;
+}
